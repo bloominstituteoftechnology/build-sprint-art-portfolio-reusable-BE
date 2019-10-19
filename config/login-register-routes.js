44f288f2ba61efc4fadbfb83ba3335d8e1472bb4
Lambda_School_Loaner_212 <lambda_school_loaner_212@LambdaSr212sMBP.localdomain>
2019-10-19 11:35:43 -0700

    secrets done

--- config/login-register-routes.js
+++ config/login-register-routes.js
@@ -1,9 +1,12 @@
 const route = require('express').Router();
-
+const  bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('../data/helpers/login-route-helper');
+const secret
 route.post('/login', (req, res) => {
 
 })
 
 route.post('/register', (req, res) => {
-    
+
 })
