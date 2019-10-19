692dc35ae0ac7e8f3317d29b398c29cb61d985be
Lambda_School_Loaner_212 <lambda_school_loaner_212@LambdaSr212sMBP.localdomain>
2019-10-19 11:30:02 -0700

    working on art routes but having a roadblock

--- config/art-routes.js
+++ config/art-routes.js
@@ -1,17 +1,32 @@
 const route = require('express').Route();
-
+const db = require('../data/helpers/art-route-helper');
 route.get('/art', restricted, (req,res) => {
-
-})
+    db.find()
+    .then(data => {
+        res.status(200).json(data);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+});
 
 route.post('/art/create', restricted, (req, res) => {
-
-})
+    const project = req.body;
+    db.add(project).then(data => {
+        res.status(200).json({
+            message: 'Posted',
+            New_Data: data
+        })
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
 
 route.put('/art/update', restricted, (req, res) => {
-
+    
 })
 
 route.remove('/art/delete', restricted, (req, res) =>{
-    
+
 })
--- data/helpers/art-route-helper.js
+++ data/helpers/art-route-helper.js
@@ -10,5 +10,29 @@ module.exports = {
 };
 
 function find(){
-    return db('art').select('');
+    return db('artwork').select('id', 'images', 'artistID');
 }
+
+function findBy(filter){
+    return db('artwork').where(filter);
+}
+
+async function add(images){
+    const [id] = await db('artwork').insert(images);
+    return findById(id);
+}
+
+function findById(id){
+    return db('artwork').select('id','images').where({id}).first();
+}
+
+function remove(id){
+    return db('artwork').where('id', id).del();
+}
+
+function update(id, changes){
+    return db('artwork').where('id', id).update(changes).then(count => {
+        count > 0 ? this.find(id) : null
+    });
+}
+
