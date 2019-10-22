4e084ed1cf7c87d98420b3d8436ae2bb7df45729
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-21 20:43:11 -0700

    fixed server but still error

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const authRoutes = require('./config/login-register-routes')
-const configureRoutes = require('./config/login-register-routes');
+const authenticate = require('./auth/authenticate')
 const artRoutes = require('./config/art-routes').default;
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use('/auth', authRoutes);
+server.use('/auth', authenticate, authRoutes );
 server.use('/art', artRoutes);
-configureRoutes(server);
+
 
 server.get('/', (req, res) => {
     res.send('Hi! Yes, hello. I am working.')
