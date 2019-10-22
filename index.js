05de0932ee59028e3aaad59c3ec9581a49d4a43f
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-22 13:06:24 -0700

    saving

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = require('./server');
 const port = process.env.PORT || 3200;
 
 server.listen(port, () => {
-    console.log(`I'm up and running on port ${port}`);
+    console.log(`It's working`);
 });
 
 
