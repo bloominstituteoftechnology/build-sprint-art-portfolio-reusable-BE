30a1c0d9c3fd42981df448111b02ee220a8a064f
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-22 13:15:43 -0700

    pg

--- knexfile.js
+++ knexfile.js
@@ -16,5 +16,31 @@ module.exports = {
       directory: './seeds'
     },
   },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './art.db3'
+    },
+    useNullAsDefault: true,
+    mifrations:{
+      directory: './migrations',
+      tableName: 'dbmigrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
 
-};
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    
+    mifrations:{
+      directory: './migrations',
+      tableName: 'dbmigrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+  }
+}
