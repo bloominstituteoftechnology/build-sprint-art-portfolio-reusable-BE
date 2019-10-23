a646b983fe6edc6d81ee95b626306ab860401b46
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-22 19:48:21 -0700

    trying something

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,6 +1,8 @@
-const knex = require('knex');
+
 
 const knexConfig = require('../knexfile.js');
-const dbENV = process.env.DB_ENV || 'development';
-module.exports = knex(knexConfig(dbENV));
+
+
+
+module.exports = knexConfig;
 
