f2e78e043db43638b7ec867d180b8c722deb1fc4
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-23 09:53:09 -0700

    converting to Postgres

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,7 @@ module.exports = {
   
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true',
+    connection: process.env.DATABASE_URL ,
     debug: true,
     pool:{
       min: 2,
