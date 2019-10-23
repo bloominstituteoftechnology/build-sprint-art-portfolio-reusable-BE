6a634b73229dac4a4c6f6c34b5db104fef8a2f49
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-23 16:50:20 -0700

    added procfile

---
+++ data/helpers/login-route-helper.js
@@ -0,0 +1,30 @@
+const db = require('../dbConfig');
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById
+};
+
+function find(){
+    return db('users').select('id', 'username', 'password');
+}
+
+function findBy(filter){
+    return db('users').where(filter);
+}
+
+async function add(user){
+    const [id] = await db('users').insert(user, 'id')
+    .returning('id');
+    const newUser = await findById(id);
+    return newUser;
+}
+
+function findById(id){
+    return db('users')
+    .where({id})
+    .first();
+}
+
