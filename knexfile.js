4a4b3099c5dbc71193f7bdeaa96678d98e7239b4
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-23 10:09:30 -0700

    ahhhhh

--- knexfile.js
+++ knexfile.js
@@ -22,10 +22,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL ,
     debug: true,
-    pool:{
-      min: 2,
-      max: 10
-    },
+   
     migrations:{
       directory: './migrations',
       
